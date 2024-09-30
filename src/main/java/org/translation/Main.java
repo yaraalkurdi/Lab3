55481063e95a24bcb7c58b65aa918c1df909c723
Yara Alkurdi <yara.alkurdi@mail.utoronto.ca>
2024-09-29 20:39:59 -0400

    To-Do's Done

--- src/main/java/org/translation/Main.java
+++ src/main/java/org/translation/Main.java
@@ -1,8 +1,9 @@
 package org.translation;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-
 /**
  * Main class for this program.
  * Complete the code according to the "to do" notes.<br/>
@@ -12,6 +13,7 @@ import java.util.Scanner;
  * - output the translation<br/>
  * - at any time, the user can type quit to quit the program<br/>
  */
+
 public class Main {
     private static final String QUIT_COMMAND = "quit";
     /**
@@ -19,76 +21,69 @@ public class Main {
      * A class implementing the Translator interface is created and passed into a call to runProgram.
      * @param args not used by the program
      */
-
     public static void main(String[] args) {
         Translator translator = new JSONTranslator(null);
-
         runProgram(translator);
     }
-
     /**
      * This is the method which we will use to test your overall program, since
      * it allows us to pass in whatever translator object that we want!
      * See the class Javadoc for a summary of what the program will do.
      * @param translator the Translator implementation to use in the program
      */
+
     public static void runProgram(Translator translator) {
         while (true) {
             String country = promptForCountry(translator);
             if (QUIT_COMMAND.equals(country)) {
                 break;
             }
+
             String language = promptForLanguage(translator, country);
             if (QUIT_COMMAND.equals(language)) {
                 break;
             }
 
-            String countryCode = getCountryCode(country);
-            String languageCode = getLanguageCode(language);
-            System.out.println(country + " in " + language + " is " + translator.translate(countryCode, languageCode));
+            System.out.println(country + " in " + la
+
+                    nguage + " is " + translator.translate(country, language));
             System.out.println("Press enter to continue or quit to exit.");
             Scanner s = new Scanner(System.in);
             String textTyped = s.nextLine();
-
             if (QUIT_COMMAND.equals(textTyped)) {
                 break;
             }
         }
     }
+    // Note: CheckStyle is configured so that we don't need javadoc for private methods
 
     private static String promptForCountry(Translator translator) {
         List<String> countries = translator.getCountries();
         List<String> countryNames = new ArrayList<>();
-        for (String countryCode : countries) {
-            countryNames.add(translator.getCountryName(countryCode));
+        for (String country : countries) {
+            countryNames.add(translator.translate(country, "en"));
         }
         Collections.sort(countryNames);
-
         for (String countryName : countryNames) {
             System.out.println(countryName);
         }
-
         System.out.println("select a country from above:");
-
         Scanner s = new Scanner(System.in);
         return s.nextLine();
     }
+    // Note: CheckStyle is configured so that we don't need javadoc for private methods
 
     private static String promptForLanguage(Translator translator, String country) {
-
-        List<String> languageCodes = translator.getCountryLanguages(country);
+        List<String> languages = translator.getCountryLanguages(country);
         List<String> languageNames = new ArrayList<>();
-        for (String languageCode : languageCodes) {
-            languageNames.add(translator.getLanguageName(languageCode));
+        for (String language : languages) {
+            languageNames.add(translator.translate(country, language));
         }
         Collections.sort(languageNames);
-
         for (String languageName : languageNames) {
             System.out.println(languageName);
         }
-
         System.out.println("select a language from above:");
-
         Scanner s = new Scanner(System.in);
         return s.nextLine();
     }
