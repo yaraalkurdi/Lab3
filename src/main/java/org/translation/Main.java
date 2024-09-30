0c3591b7369cd0971736ebdeaa6de0299dd927ea
Yara Alkurdi <yara.alkurdi@mail.utoronto.ca>
2024-09-29 20:42:08 -0400

    To-Do's Done

--- src/main/java/org/translation/Main.java
+++ src/main/java/org/translation/Main.java
@@ -44,9 +44,7 @@ public class Main {
                 break;
             }
 
-            System.out.println(country + " in " + la
-
-                    nguage + " is " + translator.translate(country, language));
+            System.out.println(country + " in " + language + " is " + translator.translate(country, language));
             System.out.println("Press enter to continue or quit to exit.");
             Scanner s = new Scanner(System.in);
             String textTyped = s.nextLine();
